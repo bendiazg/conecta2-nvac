5445fe45e25341e90d147ed9c85e596a4aaa2fc5
Unkoll <fel.zamorano@duocuc.cl>
2023-10-25 18:18:43 -0400

    Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 import { Route, Routes } from 'react-router-dom';
 import Login from './components/pages/Login';
 import Register from './components/pages/Register';
+import Home from './components/pages/Home';
 import './App.css';
-
-
-
 
 function App() {
   return (
@@ -12,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/register" element={<Register/>} />
         <Route path="/" element={<Login/>} />
+        <Route path="/home" element={<Home/>} />
       </Routes>
     </div>
 
---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,14 @@
+// Login.js
+import React, { useState } from 'react';
+import './Home.css'
+import { Link } from 'react-router-dom';
+
+
+function Home() {
+
+  return (
+    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione nam ad reprehenderit ut distinctio voluptatibus fugiat minima blanditiis odit aliquam. Et quaerat blanditiis quo temporibus laborum, corporis ab porro? Quod?</div>
+  );
+}
+
+export default Home;
